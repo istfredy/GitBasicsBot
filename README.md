8064c631e82aac15b5b3795ffc33ebdddeb1eeff
th3fr3dy <taboutsafredy@gmail.com>
2023-10-30 22:25:15 +0100

    Last configuration of README file and one update of gitbot.py file.📈

---
+++ README.md
@@ -0,0 +1,56 @@
+# Git Basics Bot
+
+This project is a boot telegram that asks questions about the theme of Git and Github.
+Directly linked to the personalized documentation that I have written and which is available on [th3fr3dy.me/gitbasics](https://th3fr3dy.me/gitbasics)
+
+The bot awaits the user response, checks its precision and goes to the following question if the answer is correct.
+
+[GitbasicsBot🤖] (https://th3fr3dy.me/gitbasics)
+
+## How it works
+
+The bot uses the [Python-Telegram-bot] library (https://python-telegram-bot.org/) to interact with the telegram API.
+It maintains a list of questions and answers, with the possibility of modifying or extending them according to your needs.
+You can start the bot and launch the command **/start **.
+
+
+## Prerequis
+
+- * Python 3.8 * or a later version
+-The library: `https://github.com/python-telegram-bot/python-telegram-bot`
+
+
+## Facility
+
+1. Clone this repository: `git clone https://github.com/th3fr3dy/Gitbasicsbot.git`
+2. Access the project directory: `cd gitbasicsbot`
+3. Install the outbuildings: `pip install -r requirements.txt`
+4. Add your Bot Telegram token to the `credential.py` or  `credential.py` file.
+
+
+## Use
+
+1. Run the `gitbot.py` script.
+2. In Telegram, find your bot and start interacting using the **/Start ** command.
+
+
+## Contribution
+
+Contributions are welcome!
+Do not hesitate to open a way out or submit a merger request.
+
+## Author
+
+*Github :* [@th3fr3dy] (https://github.com/th3fr3dy)
+*Instagram :* [@th3fr3dy] (https://instagram.com/th3fr3dy)
+*Telegram :* [@th3fr3dy] (https://t.me/th3fr3dy)
+
+## What I learned
+
+I learned to set up a ** conversation management system ** in the Bot cat.
+And I also learned to make a project modular and usable by other SWEs.
+In suppliance of small guides and documentation like this.
+
+## Licence
+
+This project is under license [MIT](https://opensource.org/licenses/mit).
--- gitbot.py
+++ gitbot.py
@@ -11,7 +11,6 @@ logging.basicConfig (
     level=logging.INFO
 )
 
-
 async def start (update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     username = update.effective_user.name
@@ -25,7 +24,7 @@ async def start (update: Update, context: ContextTypes.DEFAULT_TYPE):
         action = "typing"
     )
 
-    sleep(1) #Attends pendant une seconde pour simmuler une redaction...
+    sleep(1) # Wait for a second to simulate a redaction ...
 
     await  context.bot.send_message (
         chat_id = update.effective_chat.id,
@@ -38,49 +37,78 @@ async def start (update: Update, context: ContextTypes.DEFAULT_TYPE):
         action = "typing"
     )
 
-    sleep(1) #Attends pendant une seconde pour simmuler une redaction...
+    sleep(1) # Wait for a second to simulate a redaction ...
 
     await  context.bot.send_message (
         chat_id = update.effective_chat.id,
         text = f"Je vais vous poser une question et il vous faudrat donc trouver la solution pour avoir la question suivante. \n\n*Exemple :*\n*Question :* Comment afficher l'historique des versions pour la branche courante dans Git ?\n*Réponse attendue :* _git log_",
         reply_markup=letgoButton,
         parse_mode = "Markdown"
+
+    )
+# c_q_id === current_question_id
+c_q_id = 0
+
+async def askQuestion(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global c_q_id, Questions
+    question = f"*Question : * {Questions[0][0]}"
+
+    await context.bot.send_message (
+        chat_id = update.effective_chat.id,
+        text=  question,
+        parse_mode = "Markdown"
     )
 
-async def letgoButtonOpen (update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def getResponse(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global c_q_id, Questions
+
+    if update.message is not None and update.message.text is not None:  # Add that verification for check if update object is available for this intance 😕
+        answer = update.message.text.lower()
+        correct_answer = Questions[c_q_id][1].lower()
+
+        if correct_answer == answer:
+            c_q_id = (c_q_id + 1) % len(Questions)
+            await context.bot.send_message (
+                chat_id=update.effective_chat.id,
+                text=f"Reponse juste 👌, Passons à la Suivante.",
+                parse_mode="Markdown"
+            )
+            # Sleep for 2s
+            sleep(2)
+            await askQuestion(update=update, context=context)
+
+        else:
+            await context.bot.send_message (
+                chat_id=update.effective_chat.id,
+                text=f"Mauvaise réponse 🚨",
+                parse_mode="Markdown"
+            )
+    else:
+        await context.bot.send_message (
+            chat_id=update.effective_chat.id,
+            text=f"Un problème est survenu lors du traitement de vote requete🚨"
+        )
+        logging.warning("L'objet 'update' ou 'update.message' est None ou 'update.message.text' est None.")
+
+
+        # F-A-Q : For Ask Question
+
+async def letgoButtonOpenFAQ (update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     getQuery = update.callback_query
 
     if getQuery.data == "send_message":
-            await context.bot.send_message (
-                        chat_id= update.effective_chat.id,
-                        text=f"*Question : * {Questions[0][0]}",
-                        parse_mode= "Markdown"
-                    )
-            
+       await askQuestion (update=update, context=context)
 
-
-async def getresponse(update: Update, context: ContextTypes.DEFAULT_TYPE, answer: str):
-    if update.message.text.strip() == answer:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Reponse juste 👌",
-            parse_mode="Markdown"
-        )
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=f"Mauvaise réponse 🚨",
-            parse_mode="Markdown"
-        )
 
 if __name__ == "__main__":
 
     gitbot = ApplicationBuilder().token(TOKEN_BOT).build()
 
     startHandler = CommandHandler("start", start)
-    letgoHandler = CallbackQueryHandler(letgoButtonOpen)
-    getResponseHandler = MessageHandler(filters.TEXT & (~filters.COMMAND), getresponse)
+    letgoHandler = CallbackQueryHandler(letgoButtonOpenFAQ)
+    getResponseHandler = MessageHandler(filters.TEXT & (~filters.COMMAND), getResponse)
 
 
     gitbot.add_handler(startHandler)
